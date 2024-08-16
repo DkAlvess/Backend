77ee9fcbdd315ae406d01668cf8c76db0a30eb3e
DkAlvess <adriel.alves.senai404@gmail.com>
2024-08-16 09:32:54 -0300

    Add files via upload

---
+++ PHP/funcao_return.php
@@ -0,0 +1,10 @@
+<?php
+//Declaração de uma função que retorna um valor.
+function soma($a, $b) {
+    return $a + $b;
+}
+
+//utilizando o valor retornando pela função.
+$resultado = soma(5, 3); //$resultado pela função.
+echo $resultado; //Saída: 8
+?>
